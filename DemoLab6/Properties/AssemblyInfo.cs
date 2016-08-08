50b7a7d7ef4431a15e3e0c61bb066cef8d832199
longdg1 <longdogialong123@gmail.com>
2016-08-08 11:09:22 +0700

    demo lan 2

--- DemoLab6/Properties/AssemblyInfo.cs
+++ DemoLab6/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+//demo
 [assembly: AssemblyTitle("DemoLab6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
