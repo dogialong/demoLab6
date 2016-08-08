ddc6793678bbf91b1b3dd0cb902aa3b3daac8a03
longdg1 <longdogialong123@gmail.com>
2016-08-08 11:10:09 +0700

    demo3

--- DemoLab6/Properties/AssemblyInfo.cs
+++ DemoLab6/Properties/AssemblyInfo.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //demo
+//demo3
 [assembly: AssemblyTitle("DemoLab6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
